991aef291e3b6de0e95aece57e1fcbb53fa44e35
Pedro Henrique <pedro@cbmsolucoes.com.br>
2018-03-22 20:37:36 -0300

    #add adicionado conexao com banco e listagem de noticias

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,10 @@
+var db = require('../../config/db');
+
 module.exports = function(app) {
+    var connection = db();
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias")
+        connection.query('select * from noticias;', function(error, result){
+            res.render("noticias/noticias", {noticias: result})
+        });
     });
 }
